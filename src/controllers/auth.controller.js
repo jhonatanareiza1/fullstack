470628bf80c4eaf26bcc81a0b7412cd8835d8abb
jhonatanareiza1 <31416701+jhonatanareiza1@users.noreply.github.com>
2024-10-16 12:27:29 -0500

    validacion para proteccion de rutas establecidas

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -65,3 +65,19 @@ export const logout = (req, res) =>{
     return res.sendStatus(200);
 
 }
+
+export const profile = async (req, res) =>{
+    const userFound = await User.findById(req.user.id)
+
+    if(!userFound)
+        return res.status(404).json({message:"usuario no encontrado"})
+    
+    return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updateAt: userFound.updateAt,
+    })
+
+}
